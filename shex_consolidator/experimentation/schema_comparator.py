8b8da9c997a75edcf19a856b0eb4b141667b3070
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2023-11-15 12:46:41 +0100

    Working on comparison of resulting shapes

---
+++ shex_consolidator/experimentation/schema_comparator.py
@@ -0,0 +1,19 @@
+
+def _parse_shex_schema(shex_file_path):
+    pass  # TODO
+
+def compare_shex_files(file_path1, file_path2):
+    """
+    1º- Parse the shape schemas to in-memory objects
+    2º- Run the comparison
+
+
+    :param file_path1:
+    :param file_path2:
+    :return:
+    """
+
+    schema1 = _parse_shex_schema(file_path1)
+    schema2 = _parse_shex_schema(file_path2)
+
+    pass  # TODO
---
+++ shex_consolidator/model/schema.py
@@ -0,0 +1,32 @@
+from shex_consolidator.model import Shape
+
+
+class Schema(object):
+
+    def __init__(self, namespaces, shapes):
+        self._namespaces = namespaces
+        self._shapes = shapes
+
+    @property
+    def namespaces(self):
+        return self._namespaces
+
+    @property
+    def shapes(self):
+        return self._shapes
+
+    def yield_shapes(self):
+        for a_shape in self._shapes:
+            yield a_shape
+
+    def contains_shape(self, target_shape: Shape):
+        for a_shape in self._shapes:
+            if a_shape.label == target_shape.label:
+                return True
+        return False
+
+    def get_shape_by_label(self, shape_label: str):
+        for a_shape in self._shapes:
+            if a_shape.label == shape_label:
+                return a_shape
+        return None
